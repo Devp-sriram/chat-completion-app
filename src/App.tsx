07d83325600c3aacb0f09ee974dac7ce22f30912
Devp-sriram <sriramraman100@gmail.com>
2024-11-25 22:32:33 +0530

    query agjusted

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,16 @@ const client = new Mistral({apiKey:import.meta.env.VITE_MISTRAL_API_KEY});
 
 async function run(){
      
-     const result = await client.chat.stream({
+     const result = await client.chat.complete({
         model : 'mistral-small-latest',
         messages :[
-          { role:'system', content :"hey you are a car enthusiast you like BMW's and you know all about BMW cars , Replay with JSON" },
-          { role:'user', content : "hey do you know what is the full form of bmw" }
+          { role:'system', content :" you are programing trends analizer you analize various tech stacks and programing way to build app" },
+          { role:'user', content : "what's good for entreprice apps" }
         ],
-        temperature : 0.6,
-        
+        temperature : 0.6,  
      });
     
-    for await (const event of result) {
-    // Handle the event
-    console.log(event.data.choices[0].delta.content);
-    }
+    console.log(result.choices[0].message.content);
 }
 
   return (
